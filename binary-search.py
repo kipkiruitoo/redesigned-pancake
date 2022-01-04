29e591d97db77a9d36f1662cbad1aa92c78f4e18
Davis <daviskipkiruitoo@gmail.com>
2022-01-04 19:33:38 +0300

    updated day one

---
+++ binary-search.py
@@ -0,0 +1,23 @@
+# General Strategy for Binary Search
+# Here is the general strategy behind binary search, which is applicable to a variety of problems:
+
+#1. Come up with a condition to determine whether the answer lies before, after or at a given position
+#2. Retrieve the midpoint and the middle element of the list.
+#3. If it is the answer, return the middle position as the answer.
+#4. If answer lies before it, repeat the search with the first half of the list
+#5. If the answer lies after it, repeat the search with the second half of the list.
+
+
+
+def binary_search(lo, hi, condition):
+    """TODO - add docs"""
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        result = condition(mid)
+        if result == 'found':
+            return mid
+        elif result == 'left':
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    return -1
